4c37e5410a2eeeb161f9db27d171e40feee31e62
ji-wei-liao <77732776+ji-wei-liao@users.noreply.github.com>
2021-02-02 20:50:14 +0800

    Update D04 Numpy.py

--- homework/D04 Numpy.py
+++ homework/D04 Numpy.py
@@ -3,4 +3,12 @@ english_score = np.array([55,89,76,65,48,70])
 math_score = np.array([60,85,60,68,55,60])
 chinese_score = np.array([65,90,82,72,66,77])
 
-np.greater(english_score,math_score)
+english_ma=np.greater(english_score,math_score)
+print(english_ma)
+print(np.sum(english_ma))
+
+ch_greater_ma=np.greater(chinese_score,math_score)
+ch_greater_en=np.greater(chinese_score,english_score)
+
+print(np.logical_and(ch_greater_ma,ch_greater_en))
+print(np.logical_and(ch_greater_ma,ch_greater_en).any())
