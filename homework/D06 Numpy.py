52be3ebced73d2a78323d385e77229f54225dc0e
ji-wei-liao <77732776+ji-wei-liao@users.noreply.github.com>
2021-02-04 21:20:55 +0800

    Update D06 Numpy.py

--- homework/D06 Numpy.py
+++ homework/D06 Numpy.py
@@ -3,5 +3,9 @@ import numpy as np
 array1 = np.array(range(30))
 array2 = np.array([2,3,5])
 
-with open('multi_array.npz','wb') as f:
+with open('array.npz','wb') as f:
     np.savez(f,array1,array2)
+    
+load_array=np.load('array.npz')
+array3=np.array([[4,5,6],[1,2,3]])
+np.savez('new_array.npz',load_array,array3)
