1bcf514eeb0632443478cd279287c3c9a50d4816
ji-wei-liao <77732776+ji-wei-liao@users.noreply.github.com>
2021-02-06 21:03:23 +0800

    Update D07 Numpy.py

--- homework/D07 Numpy.py
+++ homework/D07 Numpy.py
@@ -1,4 +1,10 @@
 import numpy as np
 array1=np.array([10,8],[3,5])
 array1_inv=np.linalg.inv(array1)
-np.dot(array1_inv,arrya1)
+np.dot(array1_inv,array1)
+
+w,v=np.linalg.eig(array1)
+print(w)
+print(v)
+
+np.linalg.svd(array1)
