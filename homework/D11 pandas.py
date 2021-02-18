bb2a3435bfe415de2e9599fc7e5af94b0ff04455
ji-wei-liao <77732776+ji-wei-liao@users.noreply.github.com>
2021-02-18 20:21:30 +0800

    Create D11 pandas.py

---
+++ homework/D11 pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+q_df = pd.DataFrame([['male', 'teacher'], 
+              ['male', 'engineer'],
+              ['female', None],
+              ['female', 'engineer']],columns=['Sex','Profession'])
+
+q_df.fillna(q_df.Profession.['others'])
+print(q_df)
